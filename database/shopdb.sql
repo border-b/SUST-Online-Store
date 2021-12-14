29fe4ad80ef13333d95ab45946e2819588be63c8
border-b <babune99@gmail.com>
2021-12-14 13:37:54 +0600

    initial commit

---
+++ database/shopdb.sql
@@ -0,0 +1,500 @@
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `ecommerce`
+--
+
+DELIMITER $$
+--
+-- Procedures
+--
+CREATE DEFINER=`root`@`localhost` PROCEDURE `getcat` (IN `cid` INT)  SELECT * FROM categories WHERE cat_id=cid$$
+
+DELIMITER ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `admin_info`
+--
+
+CREATE TABLE `admin_info` (
+  `admin_id` int(10) NOT NULL,
+  `admin_name` varchar(100) NOT NULL,
+  `admin_email` varchar(300) NOT NULL,
+  `admin_password` varchar(300) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `admin_info`
+--
+
+INSERT INTO `admin_info` (`admin_id`, `admin_name`, `admin_email`, `admin_password`) VALUES
+(1, 'admin', 'admin@gmail.com', '12345678');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `brands`
+--
+
+CREATE TABLE `brands` (
+  `brand_id` int(100) NOT NULL,
+  `brand_title` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `brands`
+--
+
+INSERT INTO `brands` (`brand_id`, `brand_title`) VALUES
+(1, 'HP'),
+(2, 'Samsung'),
+(3, 'Apple'),
+(4, 'LG');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `cart`
+--
+
+CREATE TABLE `cart` (
+  `id` int(10) NOT NULL,
+  `p_id` int(10) NOT NULL,
+  `ip_add` varchar(250) NOT NULL,
+  `user_id` int(10) DEFAULT NULL,
+  `qty` int(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `cart`
+--
+
+INSERT INTO `cart` (`id`, `p_id`, `ip_add`, `user_id`, `qty`) VALUES
+(6, 26, '::1', 4, 1),
+(9, 10, '::1', 7, 1),
+(10, 11, '::1', 7, 1),
+(11, 45, '::1', 7, 1),
+(44, 5, '::1', 3, 0),
+(46, 2, '::1', 3, 0),
+(48, 72, '::1', 3, 0),
+(49, 60, '::1', 8, 1),
+(50, 61, '::1', 8, 1),
+(51, 1, '::1', 8, 1),
+(52, 5, '::1', 9, 1),
+(53, 2, '::1', 14, 1),
+(54, 3, '::1', 14, 1),
+(55, 5, '::1', 14, 1),
+(56, 1, '::1', 9, 1),
+(57, 2, '::1', 9, 1),
+(71, 61, '127.0.0.1', -1, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `categories`
+--
+
+CREATE TABLE `categories` (
+  `cat_id` int(100) NOT NULL,
+  `cat_title` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `categories`
+--
+
+INSERT INTO `categories` (`cat_id`, `cat_title`) VALUES
+(1, 'Electronics'),
+(2, 'Women'),
+(3, 'Men');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `email_info`
+--
+
+CREATE TABLE `email_info` (
+  `email_id` int(100) NOT NULL,
+  `email` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `email_info`
+--
+
+INSERT INTO `email_info` (`email_id`, `email`) VALUES
+(3, 'admin@gmail.com');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `logs`
+--
+
+CREATE TABLE `logs` (
+  `id` int(11) NOT NULL,
+  `user_id` varchar(50) NOT NULL,
+  `action` varchar(50) NOT NULL,
+  `date` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `orders`
+--
+
+CREATE TABLE `orders` (
+  `order_id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `product_id` int(11) NOT NULL,
+  `qty` int(11) NOT NULL,
+  `trx_id` varchar(255) NOT NULL,
+  `p_status` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `orders`
+--
+
+INSERT INTO `orders` (`order_id`, `user_id`, `product_id`, `qty`, `trx_id`, `p_status`) VALUES
+(1, 12, 7, 1, '07M47684BS5725041', 'Completed'),
+(2, 14, 2, 1, '07M47684BS5725041', 'Completed');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `orders_info`
+--
+
+CREATE TABLE `orders_info` (
+  `order_id` int(10) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `f_name` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `address` varchar(255) NOT NULL,
+  `city` varchar(255) NOT NULL,
+  `state` varchar(255) NOT NULL,
+  `zip` int(10) NOT NULL,
+  `cardname` varchar(255) NOT NULL,
+  `cardnumber` varchar(20) NOT NULL,
+  `expdate` varchar(255) NOT NULL,
+  `prod_count` int(15) DEFAULT NULL,
+  `total_amt` int(15) DEFAULT NULL,
+  `cvv` int(5) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `orders_info`
+--
+
+INSERT INTO `orders_info` (`order_id`, `user_id`, `f_name`, `email`, `address`, `city`, `state`, `zip`, `cardname`, `cardnumber`, `expdate`, `prod_count`, `total_amt`, `cvv`) VALUES
+(1, 12, 'Seemanta', 'babune99@gmail.com', 'Sadarghat, Chittagong', 'Bangalore', 'Bangladesh', 560074, 'pokjhgfcxc', '4321 2345 6788 7654', '12/90', 3, 77000, 1234);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `order_products`
+--
+
+CREATE TABLE `order_products` (
+  `order_pro_id` int(10) NOT NULL,
+  `order_id` int(11) NOT NULL,
+  `product_id` int(11) NOT NULL,
+  `qty` int(15) DEFAULT NULL,
+  `amt` int(15) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `order_products`
+--
+
+INSERT INTO `order_products` (`order_pro_id`, `order_id`, `product_id`, `qty`, `amt`) VALUES
+(73, 1, 1, 1, 5000),
+(74, 1, 4, 2, 64000),
+(75, 1, 8, 1, 40000);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `products`
+--
+
+CREATE TABLE `products` (
+  `product_id` int(100) NOT NULL,
+  `product_cat` int(100) NOT NULL,
+  `product_brand` int(100) NOT NULL,
+  `product_title` varchar(255) NOT NULL,
+  `product_price` int(100) NOT NULL,
+  `product_desc` text NOT NULL,
+  `product_image` text NOT NULL,
+  `product_keywords` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `products`
+--
+
+INSERT INTO `products` (`product_id`, `product_cat`, `product_brand`, `product_title`, `product_price`, `product_desc`, `product_image`, `product_keywords`) VALUES
+(1, 1, 1, 'HP Pavilion 14-dv0511sa 14" Laptop - Intel® Core™ i3, 256 GB SSD, Silver', 68990, 'Take on daily tasks, Stay powered up, Microsoft Office & internet security', 'product01.png', 'hp laptop electronics'),
+(2, 1, 3, 'Apple iPhone 12 Pro', 159900, 'Equipped with impressive features and decent specifications, the Apple iPhone 12 Pro is a perfect choice', 'iphone12.png', 'mobile iphone apple'),
+(3, 1, 2, 'Samsung Galaxy A60', 100000, 'Samsung Galaxy A60 has spiced up the game by providing triple and quad-camera setup.', 'samsunga.jpg', 'samsung galxaxy a60'),
+(4, 1, 3, 'iPhone 11', 82000, 'Apple iPhone', 'iphone11.png', 'iphone apple mobile'),
+(5, 1, 1, 'Samsung Notebook 9', 235000, 'Samsung new combination Laptop Notebook 9 Series', 'samsung_latop.jpg', 'samsung laptop'),
+(6, 1, 1, 'Laptop Pavillion', 50000, 'Laptop Hp Pavillion', 'laptop_PNG5930.png', 'Laptop Hp Pavillion'),
+(7, 3, 6, 'Sweater For Men', 1600, 'Comfy Sweater', 'sweater.png', 'sweater gents men'),
+(8, 3, 6, 'T shirt', 800, 'ssds', 'tshirt.jpg', 'formal t shirt black'),
+(9, 4, 6, 'T shirt For Kids', 1300, 'yellow t shirt', 'kidshirt.jpeg', 'kids yellow t shirt'),
+(10, 4, 6, 'Dress For Women', 2900, 'dress', 'womendress.jpeg', 'women blue dress'),
+(11, 1, 1, 'Samsung Headphones', 25000, 'Headphones', 'samsunghead.jpeg', 'Headphones Samsung'),
+(12, 3, 6, 'boys Shirts', 350, 'shirts', 'pm1.JPG', 'suit boys shirts'),
+(13, 3, 6, 'boys Shirts', 270, 'shirts', 'pm2.JPG', 'suit boys shirts'),
+(14, 3, 6, 'boys Shirts', 453, 'shirts', 'pm3.JPG', 'suit boys shirts'),
+(15, 3, 6, 'boys Shirts', 220, 'shirts', 'ms1.JPG', 'suit boys shirts');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user_info`
+--
+
+CREATE TABLE `user_info` (
+  `user_id` int(10) NOT NULL,
+  `first_name` varchar(100) NOT NULL,
+  `last_name` varchar(100) NOT NULL,
+  `email` varchar(300) NOT NULL,
+  `password` varchar(300) NOT NULL,
+  `mobile` varchar(10) NOT NULL,
+  `address1` varchar(300) NOT NULL,
+  `address2` varchar(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `user_info`
+--
+
+INSERT INTO `user_info` (`user_id`, `first_name`, `last_name`, `email`, `password`, `mobile`, `address1`, `address2`) VALUES
+(12, 'Seemanta', 'Bhattacharjee', 'babune99@gmail.com', '12345678', '01971988523', 'Chittagong', 'Sylhet');
+
+--
+-- Triggers `user_info`
+--
+DELIMITER $$
+CREATE TRIGGER `after_user_info_insert` AFTER INSERT ON `user_info` FOR EACH ROW BEGIN 
+INSERT INTO user_info_backup VALUES(new.user_id,new.first_name,new.last_name,new.email,new.password,new.mobile,new.address1,new.address2);
+END
+$$
+DELIMITER ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user_info_backup`
+--
+
+CREATE TABLE `user_info_backup` (
+  `user_id` int(10) NOT NULL,
+  `first_name` varchar(100) NOT NULL,
+  `last_name` varchar(100) NOT NULL,
+  `email` varchar(300) NOT NULL,
+  `password` varchar(300) NOT NULL,
+  `mobile` varchar(10) NOT NULL,
+  `address1` varchar(300) NOT NULL,
+  `address2` varchar(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `user_info_backup`
+--
+
+INSERT INTO `user_info_backup` (`user_id`, `first_name`, `last_name`, `email`, `password`, `mobile`, `address1`, `address2`) VALUES
+(12, 'Seemanta', 'Bhattacharjee', 'babune99@gmail.com', '12345678', '01971988523', 'Chittagong', 'Sylhet');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `admin_info`
+--
+ALTER TABLE `admin_info`
+  ADD PRIMARY KEY (`admin_id`);
+
+--
+-- Indexes for table `brands`
+--
+ALTER TABLE `brands`
+  ADD PRIMARY KEY (`brand_id`);
+
+--
+-- Indexes for table `cart`
+--
+ALTER TABLE `cart`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `categories`
+--
+ALTER TABLE `categories`
+  ADD PRIMARY KEY (`cat_id`);
+
+--
+-- Indexes for table `email_info`
+--
+ALTER TABLE `email_info`
+  ADD PRIMARY KEY (`email_id`);
+
+--
+-- Indexes for table `logs`
+--
+ALTER TABLE `logs`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `orders`
+--
+ALTER TABLE `orders`
+  ADD PRIMARY KEY (`order_id`);
+
+--
+-- Indexes for table `orders_info`
+--
+ALTER TABLE `orders_info`
+  ADD PRIMARY KEY (`order_id`),
+  ADD KEY `user_id` (`user_id`);
+
+--
+-- Indexes for table `order_products`
+--
+ALTER TABLE `order_products`
+  ADD PRIMARY KEY (`order_pro_id`),
+  ADD KEY `order_products` (`order_id`),
+  ADD KEY `product_id` (`product_id`);
+
+--
+-- Indexes for table `products`
+--
+ALTER TABLE `products`
+  ADD PRIMARY KEY (`product_id`);
+
+--
+-- Indexes for table `user_info`
+--
+ALTER TABLE `user_info`
+  ADD PRIMARY KEY (`user_id`);
+
+--
+-- Indexes for table `user_info_backup`
+--
+ALTER TABLE `user_info_backup`
+  ADD PRIMARY KEY (`user_id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `admin_info`
+--
+ALTER TABLE `admin_info`
+  MODIFY `admin_id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `brands`
+--
+ALTER TABLE `brands`
+  MODIFY `brand_id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT for table `cart`
+--
+ALTER TABLE `cart`
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=147;
+
+--
+-- AUTO_INCREMENT for table `categories`
+--
+ALTER TABLE `categories`
+  MODIFY `cat_id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
+-- AUTO_INCREMENT for table `email_info`
+--
+ALTER TABLE `email_info`
+  MODIFY `email_id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT for table `logs`
+--
+ALTER TABLE `logs`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `orders`
+--
+ALTER TABLE `orders`
+  MODIFY `order_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `orders_info`
+--
+ALTER TABLE `orders_info`
+  MODIFY `order_id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
+
+--
+-- AUTO_INCREMENT for table `order_products`
+--
+ALTER TABLE `order_products`
+  MODIFY `order_pro_id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=91;
+
+--
+-- AUTO_INCREMENT for table `products`
+--
+ALTER TABLE `products`
+  MODIFY `product_id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=82;
+
+--
+-- AUTO_INCREMENT for table `user_info`
+--
+ALTER TABLE `user_info`
+  MODIFY `user_id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=26;
+
+--
+-- AUTO_INCREMENT for table `user_info_backup`
+--
+ALTER TABLE `user_info_backup`
+  MODIFY `user_id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=26;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `orders_info`
+--
+ALTER TABLE `orders_info`
+  ADD CONSTRAINT `user_id` FOREIGN KEY (`user_id`) REFERENCES `user_info` (`user_id`);
+
+--
+-- Constraints for table `order_products`
+--
+ALTER TABLE `order_products`
+  ADD CONSTRAINT `order_products` FOREIGN KEY (`order_id`) REFERENCES `orders_info` (`order_id`) ON DELETE NO ACTION ON UPDATE CASCADE,
+  ADD CONSTRAINT `product_id` FOREIGN KEY (`product_id`) REFERENCES `products` (`product_id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
